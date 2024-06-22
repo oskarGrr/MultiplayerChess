c6e779ad25392b1d1cbca13379b91e4c42b53235
Grant <acdcgrr@gmail.com>
2024-06-21 23:57:54 -0500

    changed event system to be more strict with event types

--- src/hpp/ChessEvents.hpp
+++ src/hpp/ChessEvents.hpp
@@ -1,59 +1,51 @@
-#pragma once
-#include <string>
-#include <string_view>
 #include <functional>
-#include <utility>//std::move() std::pair
-#include <typeindex>
 #include <unordered_map>
+#include <typeindex>
 
-struct Event
-{
-protected:
-    virtual ~Event()=default;
-};
+//no virtual dtor because runtime polymorphism is not used
+struct Event {};
 
-template <typename T>
-concept ChessEvent = std::is_base_of_v<Event, T>;
+template <typename T, typename... Types>
+concept IsTypeInPack = (std::is_same_v<T, Types> || ...);
 
-//Extremely simple syncronous publisher/subscriber event system.
-//Events are not put in a queue. They are handled immediately.
-class PubSubEventSystem
+template <typename... EventTs>
+class EventSystem
 {
 public:
 
-    using OnEventCallback = std::function<void(Event&)>;
+    static_assert((std::is_base_of_v<Event, EventTs> && ...),
+        "All event types must inherit from Event (no runtime polymorphism)");
 
-    template <ChessEvent>
-    void subscribe(OnEventCallback callback)
+    using Callable_t = std::function<void(Event&)>;
+
+    template <typename EventType>
+    void subscribe(Callable_t callback)
     {
-        //Get a reference to the vector of functions for this event type
-        auto const& functions {mSubscriptions.try_emplace(
-            typeid(IsChessEvent)).first->second};
+        //static assert used instead of a requires constraint to allow for a helpful error message.
+        static_assert(IsTypeInPack<EventType, EventTs...>, "The event type template paramater passed to"
+            " EventSystem::subscribe is not a valid event type for this EventSystem." 
+            " The list of valid event types are passed in to the variadic class template EventSystem");
 
-        functinos.push_back(std::move(callback));
+        auto& functions {mSubscriptions.try_emplace(typeid(EventType)).first->second};
+        functions.push_back(std::move(callback));
     }
 
-    template <ChessEvent EventType>
-    void publish(EventType& evnt)
+    template <typename EventType>
+    void publish(EventType& e)
     {
-        auto const it = mSubscriptions.find(typeid(evnt));
+        //static assert used instead of a requires constraint to allow for a helpful error message.
+        static_assert(IsTypeInPack<EventType, EventTs...>, "The event type template paramater passed to"
+            " EventSystem::publish is not a valid event type for this EventSystem." 
+            " The list of valid event types are passed in to the variadic class template EventSystem");
+
+        auto const it = mSubscriptions.find(typeid(e));
 
         if(it != mSubscriptions.end())
         {
             for(auto const& fn : it->second)
-                fn(evnt);
+                fn(e);
         }
     }
 
-private:
-
-    std::unordered_map<std::type_index, std::vector<OnEventCallback>> mSubscriptions;
-
-public:
-    PubSubEventSystem()=default;
-    ~PubSubEventSystem()=default;
-    PubSubEventSystem(PubSubEventSystem const&)=delete;
-    PubSubEventSystem(PubSubEventSystem&&)=delete;
-    PubSubEventSystem& operator=(PubSubEventSystem const&)=delete;
-    PubSubEventSystem& operator=(PubSubEventSystem&&)=delete;
+    std::unordered_map< std::type_index, std::vector<Callable_t> > mSubscriptions;
 };
